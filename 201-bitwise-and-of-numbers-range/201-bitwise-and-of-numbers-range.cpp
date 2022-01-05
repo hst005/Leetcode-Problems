378ce4be9acfff986fbed8d0f95ab535a64b6ec6
Harsimran Singh <harsimransingh05@gmail.com>
2022-01-06 03:23:38 +0530

    Time: 15 ms (33.59%), Space: 5.9 MB (31.55%) - LeetHub

---
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count=0;
+        while(left!=right){
+            count++;
+            left>>=1;
+            right>>=1;
+            
+        }
+        return left<<count;
+    }
+};
